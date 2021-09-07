50a8db2f93237c9aceae28179e76038fd488afdb
Adan <adanfrancisco@gmail.com>
2021-09-07 01:02:35 -0300

    vars

--- src/redux/vars.js
+++ src/redux/vars.js
@@ -2,3 +2,4 @@ export const uri_local = "https://is93.aloeadan.com/api/alumnos/";
 // export const uri = "https://is93.aloeadan.com/api/alumnos/";
 // export const uri_local ="http://localhost/api/alumnos/";
 export const logo = "https://isfdyt93-bue.infd.edu.ar/sitio/wp-content/uploads/2019/10/INI2.jpeg";
+
